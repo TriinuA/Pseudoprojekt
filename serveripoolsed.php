d1eb2ae426abc23645d9d3ce3c544bc07c5a6803
TriinuA <triinu.a@gmail.com>
2015-09-12 19:59:21 +0300

    Stringid [Delivers #103249210]

---
+++ serveripoolsed.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Serveripoolsed</title>
+</head>
+<body>
+     <?php
+        $eesnimi = "Triinu";
+        $perekonnanimi = "Ausmees";
+
+        echo("$eesnimi $perekonnanimi");
+     ?>
+</body>
+</html>
