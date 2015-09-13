5e9256a66d2eddbe866f77d2070a61036a9ed1a9
TriinuA <triinu.a@gmail.com>
2015-09-13 13:44:07 +0300

    Täisarvud [Delivers #103249218] and improved international

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,10 +6,16 @@
 </head>
 <body>
      <?php
-        $eesnimi = "Triinu";
-        $perekonnanimi = "Ausmees";
 
-        echo("$eesnimi $perekonnanimi");
+        $first_name = "Triinu";
+        $last_name = "Ausmees";
+        $birth_year	= 1979;
+        $this_year 	= date('Y');
+        $my_age = ($this_year - $birth_year);
+
+        echo("$first_name $last_name (").$my_age.(')');
      ?>
+
 </body>
 </html>
+
