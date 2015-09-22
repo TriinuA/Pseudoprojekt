21857b6d98b2caab9a8c7f98752b0414cb8333df
TriinuA <triinu.a@gmail.com>
2015-09-22 14:05:24 +0300

    16.Massiivid [Delivers #103249222]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -13,9 +13,12 @@
         $this_year 	= date('Y');
         $my_age = ($this_year - $birth_year);
         $first_name_begins_vowel = false;
+        $persons = array($first_name, $last_name, $my_age,);
 
         echo("$first_name $last_name (").$my_age.(')');
      ?>
+
+     <p><?php echo $persons[0]." ". $persons[1]."  (".$persons[2].")"?> </p>
 
 </body>
 </html>
