a563cd86fa4c8a04028b53c6907d7a6be24c4d8d
TriinuA <triinu.a@gmail.com>
2015-09-22 14:22:16 +0300

    3.Tere maailm link [Delivers #103057402]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -9,11 +9,20 @@
         Tere maailm!
     </button>
 
+    <ul>
+        <li><a href="http://www.khk.ee" id="alertLink">Tere maailm!</a></li><!--3. Tere maailm link-->
+    </ul>
+
 
     <script>
         var oneButton = document.getElementById("alertButton"); //2. Tere maailm button
         oneButton.onclick = function() {
             alert("Tere maailm!");
+        }
+
+        var oneClick = document.getElementById("alertLink");  //3. Tere maailm link
+        oneClick.onclick = function() {
+            alert("Tere maailm");
         }
 
     </script>
