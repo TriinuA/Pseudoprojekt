0dbfb332be8b8ec3451fb4339cdd65911e4d6b9a
TriinuA <triinu.a@gmail.com>
2015-09-22 14:16:43 +0300

    2.Tere maailm button [Delivers #103057386]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,6 +5,18 @@
     <title>Kliendipoolsed</title>
 </head>
 <body>
+    <button type="button" id="alertButton">
+        Tere maailm!
+    </button>
+
+
+    <script>
+        var oneButton = document.getElementById("alertButton"); //2. Tere maailm button
+        oneButton.onclick = function() {
+            alert("Tere maailm!");
+        }
+
+    </script>
 
 </body>
 </html>
